c0864e8371ed34122eb01b8efa82e65f635f1d5d
eci4ever <eci4ever@gmail.com>
2025-09-05 18:16:02 +0800

    feat: add customer fetching API endpoint

---
+++ app/api/view-data/route.ts
@@ -0,0 +1,22 @@
+import { NextResponse } from "next/server";
+import sql from "@/app/lib/db";
+
+// Fetch Customers
+async function getCustomers() {
+  const customers = await sql`
+    SELECT * FROM customers;
+  `;
+  return customers;
+}
+
+export async function GET() {
+  try {
+    // Fetch customers after seeding
+    const customers = await getCustomers();
+
+    return Response.json({ message: "Get customers successfully ✅", customers });
+  } catch (error: any) {
+    console.error(error);
+    return Response.json({ error: error.message }, { status: 500 });
+  }
+}
