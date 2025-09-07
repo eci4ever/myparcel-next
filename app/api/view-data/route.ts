31fbd2a84bf05d17fd7449c342bc823ae4a9f698
eci4ever <eci4ever@gmail.com>
2025-09-07 11:18:07 +0800

    Refactor imports to use new utility path and remove deprecated utils file; add new data fetching functions for revenue, invoices, and customers in app/lib/data.ts; implement currency and date formatting utilities in app/lib/utils.ts.

--- app/api/view-data/route.ts
+++ app/api/view-data/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import sql from "@/app/lib/db";
-
+import { fetchLatestInvoices } from "@/app/lib/data";
 // Fetch Customers
 async function getCustomers() {
   const customers = await sql`
@@ -12,7 +12,7 @@ async function getCustomers() {
 export async function GET() {
   try {
     // Fetch customers after seeding
-    const customers = await getCustomers();
+    const customers = await fetchLatestInvoices();
 
     return Response.json({
       message: "Get customers successfully ✅",
--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,14 +1,18 @@
-import { DataTable } from "@/components/data-table"
-import { DataTableDemo } from "@/components/invoices/data-table"
+import { DataTable } from "@/components/invoices/data-table";
+import { fetchLatestInvoices } from "@/app/lib/data"
+import { fetchAllInvoices } from "@/app/lib/data";
 
-export default function InvoicesPage() {
+
+export default async function InvoicesPage() {
+  const data = await fetchAllInvoices();
+  console.log("Latest Invoices:", data);
   return (
         <div className="flex flex-1 flex-col">
           <div className="@container/main flex flex-1 flex-col gap-2">
             <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
               {/* <SectionCards /> */}
               <div className="px-4 lg:px-6">
-                {/* <ChartAreaInteractive /> */} <DataTableDemo />
+                <DataTable data={data} />
               </div>
              
             </div>
